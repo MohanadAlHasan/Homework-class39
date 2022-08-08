19d258d6eb4e20abc95e6b95643c44ae0297329d
Mohanad <mohanad.hnk@gmil.com>
2022-08-08 20:04:08 +0200

    Do ex5

--- 1-JavaScript/Week3/homework/ex5-shoppingCartPure.js
+++ 1-JavaScript/Week3/homework/ex5-shoppingCartPure.js
@@ -16,8 +16,17 @@ it pure. Do the following:
 5. Confirm that you function passes the provided unit tests.
 ------------------------------------------------------------------------------*/
 // ! Function under test
-function addToShoppingCart(/* TODO parameter(s) go here */) {
-  // TODO complete this function
+function addToShoppingCart(arr, grocery) {
+  let shoppingCart = [...arr];
+  if (grocery === undefined) {
+    return arr;
+  } else shoppingCart = [...arr, grocery];
+
+  if (shoppingCart.length > 3) {
+    shoppingCart.shift();
+  }
+
+  return shoppingCart;
 }
 
 // ! Test functions (plain vanilla JavaScript)
